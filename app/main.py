47ce451141ffc62e8c1a9a40612f9fe53dcbd818
Alexscdt <alex.schmidt59@icloud.com>
2024-10-21 00:27:32 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,13 +4,22 @@ def create_message(id):
     id_bytes = id.to_bytes(4, byteorder="big")
     return len(id_bytes).to_bytes(4, byteorder="big") + id_bytes
 
+def create_error_message(coRelationId, error_code):
+    message = coRelationId.to_bytes(4, byteorder="big") + error_code.to_bytes(2, byteorder="big")
+    return len(message).to_bytes(4, byteorder="big") + message
+
 def handle_client(client):
     req = client.recv(1024)
-    print(req)
+    acceptApiVersion = [0,1,2,3,4]
+    api_version = int.from_bytes(req[6:8], byteorder="big")
     coRelationId = int.from_bytes(req[8:12], byteorder="big")
 
-    if coRelationId > 4:
-        raise Exception("Invalid coRelationId")
+    if api_version not in acceptApiVersion:
+        print('api_version not in acceptApiVersion')
+        error_code = 35
+        client.sendall(create_error_message(coRelationId, error_code))
+        client.close()
+        return
 
     client.sendall(create_message(coRelationId))
     client.close()
