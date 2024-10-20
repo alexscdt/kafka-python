6519dc98567ea667be4539e842a3b01f5055fe96
Alexscdt <alex.schmidt59@icloud.com>
2024-10-20 04:21:32 +0200

    Send Correlation ID

--- app/main.py
+++ app/main.py
@@ -1,11 +1,22 @@
 import socket  # noqa: F401
 
+def create_message(id):
+    id_bytes = id.to_bytes(4, byteorder="big")
+    return len(id_bytes).to_bytes(4, byteorder="big") + id_bytes
+
+def handle_client(client):
+    client.recv(1024)
+    client.sendall(create_message(7))
+    client.close()
 
 def main():
     print("Logs from your program will appear here!")
 
     server = socket.create_server(("localhost", 9092), reuse_port=True)
-    server.accept()
+
+    while True:
+        client, addr = server.accept()
+        handle_client(client)
 
 
 if __name__ == "__main__":
